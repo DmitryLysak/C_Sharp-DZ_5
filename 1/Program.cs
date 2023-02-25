911b0b033ea275a02a76cd890f6147fc263f0ba7
Dmitry Lysak <lysak.d@gmail.com>
2023-02-25 17:55:32 +0300

    added 1

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] array = randomArray(4); 
+Console.WriteLine($"[{string.Join(", ",array)}] -> {sumOfEvenNumbers(array)}"); 
+
+ int[] randomArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next(1000);
+    }
+    return result;
+}
+int sumOfEvenNumbers(int[] array) 
+{ 
+ int sum = 0; 
+ for(int i = 0; i < array.Length; i++) { 
+ if(array[i] % 2 == 0) 
+{
+ sum = sum+1; 
+}    
+    }
+return sum;
+}
+
